1dfe28ff617736a3ed2cb7cce1499cd5543a8ca3
FahimFBA <fahimbinamin@gmail.com>
2023-02-07 00:26:50 +0600

    two sum in O(n)

--- 1. Two Sum/solution.py
+++ 1. Two Sum/solution.py
@@ -1,2 +1,17 @@
 # Time = O(N) and Space = O(N)
 # Using Hash Table
+
+def twoNumberSum(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    return []
+
+# driver code
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+targetSum = 19
+print(twoNumberSum(array, targetSum))
